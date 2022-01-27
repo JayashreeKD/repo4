35306287541ceb3b34d06cc9f653238347f3757f
admin <admin@DESKTOP-7E5APTE>
2022-01-27 23:01:09 +0530

    comit     by 1st user

---
+++ src/test/java/com/practice/GitTest.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+import org.testng.annotations.Test;
+
+public class GitTest {
+	
+	
+	@Test
+	public void gitTest()
+	{
+		System.out.println("insde 1st git ");
+	}
+	
+
+}
